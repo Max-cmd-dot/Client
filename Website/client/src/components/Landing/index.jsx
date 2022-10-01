ea54cce90aa9e89c30919aa713bff57b83e28319
Maximilian Nobis <Maximilian.nobis@icloud.com>
2022-10-01 16:46:48 +0000

    landing look stable div sensor+ diagramm

--- Website/client/src/components/Landing/index.jsx
+++ Website/client/src/components/Landing/index.jsx
@@ -1,33 +1,33 @@
 import styles from "./styles.module.css";
 import { Link } from "react-router-dom";
-import image from 'c://Programm_React/Website/client/src/components/Pictures/sensor.png';
-import image2 from 'c://Programm_React/Website/client/src/components/Pictures/module.png';
+import image from "c://Programm_React/Website/client/src/components/Pictures/sensor2.png";
+import image2 from "c://Programm_React/Website/client/src/components/Pictures/module2.png";
 const Main = () => {
-//Iot-Garden Automatisierung
-	
-	return (
+  //Iot-Garden Automatisierung
 
-		<div>
-            <div className={styles.main_container}>
-				<nav className={styles.navbar}>
-					<h1>Iot-Garden  Planting</h1>
-					<Link to="/login">
-						<button type="button" className={styles.white_btn}>
-							Log in
-						</button>
-					</Link>
-				</nav>
-		
-				<div className={styles.picture1}>
-					<img src={image} alt="Sensor im Boden" className={styles.img} />
-					<h2 className={styles.infotext}>Iot-Garden</h2>
-				</div>
-				<div className={styles.picture2}>
-					<img src={image2} alt="Sensor Controller" className={styles.img} />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className={styles.main_container}>
+        <nav className={styles.navbar}>
+          <h1>Plant</h1>
+          <Link to="/login">
+            <button type="button" className={styles.white_btn}>
+              Log in
+            </button>
+          </Link>
+        </nav>
+
+        <div className={styles.picture1}>
+          <img src={image} alt="Sensor im Boden" className={styles.img} />
+          <h2 className={styles.info3words}>Future, Planting, Efficiency</h2>
+        </div>
+        <div className={styles.picture2}>
+          <img src={image2} alt="Sensor Controller" className={styles.img} />
+          <h3 className={styles.infotext}>Text about lorem ipsum dolor</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
