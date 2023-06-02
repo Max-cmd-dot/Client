92f4f9302291961d23c8a408f701b9a9e1c69d30
Maximilian Nobis <60576883+Max-cmd-dot@users.noreply.github.com>
2023-06-02 20:47:54 +0200

    website backend try fix for vercel

--- Website/server/db.js
+++ Website/server/db.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 module.exports = () => {
   const connectionParams = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   };
   try {
+    console.log(process.env.DB);
     mongoose.connect(process.env.DB, connectionParams);
     console.log("Connected to database successfully");
   } catch (error) {
--- Website/server/index.js
+++ Website/server/index.js
@@ -5,13 +5,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connection = require("./db");
-const userRoutes = require("./routes/users");
-const authRoutes = require("./routes/auth");
-const dataRoutes = require("./routes/apidata");
-const password_resetRoutes = require("./routes/passwordReset");
-const notificationRoutes = require("./routes/notificationdata");
-const apiuserdataRoutes = require("./routes/apiuserdata");
-const groupRoutes = require("./routes/groupRoutes");
+const userRoutes = require("./src/routes/users");
+const authRoutes = require("./src/routes/auth");
+const dataRoutes = require("./src/routes/apidata");
+const password_resetRoutes = require("./src/routes/passwordReset");
+const notificationRoutes = require("./src/routes/notificationdata");
+const apiuserdataRoutes = require("./src/routes/apiuserdata");
+const groupRoutes = require("./src/routes/groupRoutes");
 
 // database connection
 connection();
