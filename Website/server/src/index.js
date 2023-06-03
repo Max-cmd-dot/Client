dfef07c6acdfa7d810a441b4233755a7152b7cf2
Maximilian Nobis <60576883+Max-cmd-dot@users.noreply.github.com>
2023-06-03 02:59:51 +0200

    vercel fix

--- Website/server/src/index.js
+++ Website/server/src/index.js
@@ -4,14 +4,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const connection = require("./db");
-const userRoutes = require("./src/routes/users");
-const authRoutes = require("./src/routes/auth");
-const dataRoutes = require("./src/routes/apidata");
-const password_resetRoutes = require("./src/routes/passwordReset");
-const notificationRoutes = require("./src/routes/notificationdata");
-const apiuserdataRoutes = require("./src/routes/apiuserdata");
-const groupRoutes = require("./src/routes/groupRoutes");
+const connection = require("../db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+const dataRoutes = require("./routes/apidata");
+const password_resetRoutes = require("./routes/passwordReset");
+const notificationRoutes = require("./routes/notificationdata");
+const apiuserdataRoutes = require("./routes/apiuserdata");
+const groupRoutes = require("./routes/groupRoutes");
 
 // database connection
 connection();
@@ -36,8 +36,8 @@ const options = {
   key: fs.readFileSync("key.pem"),
   cert: fs.readFileSync("cert.pem"),
 };
-const port = process.env.PORT || 8080;
+//const port = process.env.PORT || 8080;
 //app.listen(port, console.log(`Listening on port ${port}...`));
-https
-  .createServer(options, app)
-  .listen(port, console.log(`server runs on port ${port}`));
+//https
+//.createServer(options, app)
+//.listen(port, console.log(`server runs on port ${port}`));
