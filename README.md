4cd008bcb9132a90943b396aa9fc25cafc1e0997
Maximilian Nobis <max.nobishh@gmail.com>
2023-12-19 13:00:01 +0100

    save settings from notifications

--- README.md
+++
@@ -1,8 +0,0 @@
-# BLL
-https://nexaharvest.com/landing
-
-# installation
-- install mosquitto
-- install paho-mqtt
-- edit config of mosquitto -> allow anonymus, listener 1883
-- in /website/client + /website/client -> npm install
--- Website/server/src/models/group.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const groupSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-});
-
-const Group = mongoose.model("Group", groupSchema);
-
-module.exports = { Group };
